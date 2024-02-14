c99b7134c453bd21fd1be014a6f2567d3f85e3b3
Marat Faizerakhmanov <maratismodest@gmail.com>
2024-02-14 22:29:11 +0300

    update

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,17 @@ import Categories from '@/components/Categories';
 import InfinitePosts from '@/modules/InfinitePosts';
 import fetchAds from '@/utils/api/fetchAds';
 import fetchCategories from '@/utils/api/fetchCategories';
+import { WebSite, WithContext } from 'schema-dts';
 
 export const revalidate = 3600;
+
+const jsonLd: WithContext<WebSite> = {
+  ['@context']: 'https://schema.org',
+  '@type': 'WebSite',
+  name: 'InnoAds',
+  alternateName: 'Innopolis Classified',
+  url: 'https://innoads.ru',
+};
 
 export default async function Home() {
   const { content: posts, totalPages } = await fetchAds({
@@ -14,6 +23,10 @@ export default async function Home() {
 
   return (
     <>
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+      />
       <Categories categories={categories} />
       <div className="flex justify-between align-baseline">
         <h1>Последние объявления</h1>
