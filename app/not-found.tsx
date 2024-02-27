78e7fc4cb3fbcfa08b323f40a351c4a46aa50e66
Marat Faizerakhmanov <maratismodest@gmail.com>
2024-02-27 10:52:17 +0300

    rid of telegram script

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,4 +1,3 @@
-import Button from '@/components/ui/Button';
 import buttonStyles from '@/styles/buttonStyles';
 import { routes } from '@/utils/constants';
 import Image from 'next/image';
@@ -6,10 +5,12 @@ import Link from 'next/link';
 
 export default function NotFoundPage() {
   return (
-    <div className='flex justify-center items-center flex-col flex-1 gap-2'>
-      <Image src='/images/error-404.png' alt='404' width={128} height={128} />
+    <div className="flex flex-1 flex-col items-center justify-center gap-2">
+      <Image src="/images/error-404.png" alt="404" width={128} height={128} />
       <h1>Страница не найдена</h1>
-      <Link className={buttonStyles()} href={routes.main}>На главную</Link>
+      <Link className={buttonStyles()} href={routes.main}>
+        На главную
+      </Link>
     </div>
   );
 }
