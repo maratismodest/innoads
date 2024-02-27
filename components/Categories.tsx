9d3d04669623efdb13c009bcaedcc15398776585
Marat Faizerakhmanov <maratismodest@gmail.com>
2024-02-27 11:04:02 +0300

    Update Categories.tsx

--- components/Categories.tsx
+++ components/Categories.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 
 interface CategoriesProp {
-  categories: CategoryDTO[];
+  categories: Readonly<CategoryDTO>[];
 }
 
 export default function Categories({ categories }: CategoriesProp) {
