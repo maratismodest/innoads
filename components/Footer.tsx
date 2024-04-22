d4c2293aebac8f3be7a53b3b922c1cf702a43d70
Marat Faizerakhmanov <maratismodest@gmail.com>
2024-04-22 23:58:05 +0300

    make footer server component

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,12 +1,16 @@
-'use client';
-import Button from '@/components/ui/Button';
-import scrollToTop from '@/utils/scrollToTop';
+// 'use client';
+// import Button from '@/components/ui/Button';
+import buttonStyles from '@/styles/buttonStyles';
+// import scrollToTop from '@/utils/scrollToTop';
+import clsx from 'clsx';
 import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className='bg-gray'>
-      <Button variant='secondary' className='w-full' onClick={scrollToTop}>Наверх</Button>
+    <footer className="bg-gray">
+      <a className={clsx(buttonStyles({ variant: 'secondary' }), 'w-full')} href="#">
+        Наверх
+      </a>
     </footer>
   );
 };
