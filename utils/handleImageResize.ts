6c2b48ac882802e9930eec08a03daa8768eedb78
Marat Faizerakhmanov <maratismodest@gmail.com>
2024-04-26 00:27:52 +0300

    reduce original image size

--- utils/handleImageResize.ts
+++ utils/handleImageResize.ts
@@ -1,7 +1,7 @@
 import imageCompression from 'browser-image-compression';
 
 const options = {
-  maxSizeMB: 1,
+  maxSizeMB: 0.9,
   maxWidthOrHeight: 800,
   useWebWorker: true,
 };
@@ -9,4 +9,3 @@ const options = {
 export default async function handleImageResize(imageFile: File) {
   return await imageCompression(imageFile, options);
 }
-
