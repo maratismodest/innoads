dac93616be399ff933744cf73cd03a28f8a840a5
Marat Faizerakhmanov <maratismodest@gmail.com>
2024-05-18 11:23:34 +0300

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -7,28 +7,37 @@ import dayjs from 'dayjs';
 
 const URL = process.env.NEXT_PUBLIC_APP_URL;
 
-const mainRoutes = [routes.main, routes.blog, routes.favourites, routes.add, routes.profile, routes.search];
+const mainRoutes = [
+  routes.main,
+  routes.blog,
+  routes.favourites,
+  routes.add,
+  routes.profile,
+  routes.search,
+];
 
 const formatDate = (date: string) => dayjs(date).format(dateFormat.time);
 
 export default async function sitemap() {
-  const routes = mainRoutes.map(route => ({
-    url: `${URL}${route}`,
-    lastModified: formatDate(new Date().toISOString()),
-  }));
+  // const routes = mainRoutes.map(route => ({
+  //   url: `${URL}${route}`,
+  //   lastModified: formatDate(new Date().toISOString()),
+  // }));
+  //
+  // const { content } = await fetchAds({ size: 1000 });
+  //
+  // const posts = content.map(({ slug, updatedAt }) => ({
+  //   url: `${URL}/post/${slug}`,
+  //   lastModified: formatDate(updatedAt),
+  // }));
+  //
+  // const articles = await fetchArticles();
+  // const articleRoutes = articles.map(({ slug, updatedAt }) => ({
+  //   url: `${URL}/blog/${slug}`,
+  //   lastModified: formatDate(updatedAt),
+  // }));
+  //
+  // return [...routes, ...posts, ...articleRoutes];
 
-  const { content } = await fetchAds({ size: 1000 });
-
-  const posts = content.map(({ slug, updatedAt }) => ({
-    url: `${URL}/post/${slug}`,
-    lastModified: formatDate(updatedAt),
-  }));
-
-  const articles = await fetchArticles();
-  const articleRoutes = articles.map(({ slug, updatedAt }) => ({
-    url: `${URL}/blog/${slug}`,
-    lastModified: formatDate(updatedAt),
-  }));
-
-  return [...routes, ...posts, ...articleRoutes];
+  return [];
 }
