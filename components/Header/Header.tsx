810a0c2cf8765fbf158ef27ae4f52c94862ca919
Marat Faizerakhmanov <maratismodest@gmail.com>
2024-05-18 11:46:48 +0300

    Update Header.tsx

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -11,7 +11,7 @@ export default function Header() {
         <Link href={routes.main} className="flex items-center gap-2">
           <span className="text-2xl uppercase">{process.env.NEXT_PUBLIC_APP_NAME}</span>
           <span className="hidden lg:inline">|</span>
-          <span className="hidden lg:inline">Доска объявлений города Иннополис</span>
+          <span className="hidden lg:inline">Доска объявлений города София</span>
         </Link>
         <HeaderMobileMenu />
         <div className="hidden items-center gap-2 lg:flex">
