eb5d8b0269f2aa1402e5a81aebf35a896235b03d
Marat Faizerakhmanov <maratismodest@gmail.com>
2024-05-18 14:34:48 +0300

    Update route.ts

--- app/api/users/login/route.ts
+++ app/api/users/login/route.ts
@@ -3,7 +3,6 @@ import { NextResponse } from 'next/server';
 
 export async function POST(request: Request) {
   const res = await request.json();
-  console.log('res', res);
   const token = await getToken(res);
 
   return NextResponse.json({ token });
