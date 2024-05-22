17aaa4b1c228fbf64bf8a8b78cc8330d8a3f5dda
Marat Faizerakhmanov <maratismodest@gmail.com>
2024-05-22 12:57:43 +0300

    add admin

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,28 +1,55 @@
 'use client';
-import Users from '@/pages-lib/admin/users';
+import Posts from '@/components/Posts';
+import useAuth from '@/hooks/useAuth';
+import buttonStyles from '@/styles/buttonStyles';
 import fetchBansApi from '@/utils/api/fetchBansApi';
 import fetchUsersApi from '@/utils/api/fetchUsersApi';
-// import fetchBans from '@/utils/api/prisma/fetchBans';
-// import fetchUsers from '@/utils/api/prisma/fetchUsers';
-import { Ban, User } from '@prisma/client';
+import fetchPosts from '@/utils/api/prisma/fetchAds';
+import { Ban, Post, User } from '@prisma/client';
 import React, { useEffect, useState } from 'react';
 
 export default function Page() {
+  const { user } = useAuth();
+  console.log('user', user);
   const [users, setUsers] = useState<User[]>([]);
   const [bans, setBans] = useState<Ban[]>([]);
+  const [posts, setPosts] = useState<Post[]>([]);
 
-  // console.log('users', users);
-  // console.log('bans', bans);
-
-  useEffect(() => {
+  const onClick = () => {
     fetchUsersApi().then(res => setUsers(res));
     fetchBansApi().then(res => setBans(res));
-  }, []);
+    fetchPosts({ size: 500 }).then(res => setPosts(res));
+  };
+  useEffect(() => {
+    if (user && user.role === 'ADMIN') {
+      onClick();
+    }
+  }, [user]);
+
+  if (!user || user.role !== 'ADMIN') {
+    return (
+      <div>
+        <h1>У вас нет доступа к этой странице!</h1>
+      </div>
+    );
+  }
 
   return (
     <div>
-      <Users users={users} bans={bans} />
+      {/*<Users users={users} bans={bans} />*/}
+      {/*<hr />*/}
+      <div className="mb-2 flex justify-between">
+        <h1>Посты</h1>
+        <button className={buttonStyles()} onClick={onClick}>
+          Обновить данные
+        </button>
+      </div>
+
+      <p>Можно с помощью крестика снять объявление из публикации</p>
+      <Posts posts={posts.filter(x => x.published === true)} edit={true} />
       <hr />
+      <h2>Архивные</h2>
+      <Posts posts={posts.filter(x => x.published === false)} edit={true} />
     </div>
   );
 }
