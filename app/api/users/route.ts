6e6cc7137ad92e140589683e698305722951e976
Marat Faizerakhmanov <maratismodest@gmail.com>
2024-05-22 22:18:21 +0300

    Update route.ts

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,13 +1,15 @@
 import { getAllUsers } from '@/prisma/services/users';
+import { User } from '@prisma/client';
 import { NextResponse } from 'next/server';
+
+const prepareUser = (user: User) =>
+  JSON.stringify(user, (_key, value) => {
+    typeof value === 'bigint' ? (value = value.toString()) : value;
+  });
 
 export async function GET() {
   const res = await getAllUsers();
-  const updatedData = res.map(x =>
-    JSON.stringify(res, (_key, value) => {
-      typeof value === 'bigint' ? (value = value.toString()) : value;
-    })
-  );
+  const preparedUsers = res.map(user => prepareUser(user));
 
-  return NextResponse.json(updatedData);
+  return NextResponse.json(preparedUsers);
 }
