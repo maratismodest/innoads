c63f4898798b07731b8c71d21110120dfa3c930a
Marat Faizerakhmanov <maratismodest@gmail.com>
2024-05-25 19:34:37 +0300

    update

--- app/add/page.tsx
+++ app/add/page.tsx
@@ -1,12 +1,24 @@
 'use client';
+import Spinner from '@/components/ui/Spinner';
+import useAuth from '@/hooks/useAuth';
 import CreatePostModule from '@/modules/PostModule/CreatePostModule/CreatePostModule';
+import ProfileNoUser from '@/pages-lib/profile/ProfileNoUser';
 import { routes } from '@/utils/constants';
 import { useRouter } from 'next/navigation';
-import { useCallback } from 'react';
+import React, { useCallback } from 'react';
 
 export default function AddPage<NextPage>() {
+  const { user, loading: userLoading } = useAuth();
   const router = useRouter();
   const onSubmitOptional = useCallback(async () => router.push(routes.profile), []);
+
+  if (userLoading) {
+    return <Spinner />;
+  }
+
+  if (!user) {
+    return <ProfileNoUser />;
+  }
 
   return <CreatePostModule onSubmitOptional={onSubmitOptional} />;
 }
--- app/bot/page.tsx
+++ app/bot/page.tsx
@@ -1,7 +1,24 @@
 'use client';
+import Spinner from '@/components/ui/Spinner';
+import useAuth from '@/hooks/useAuth';
 import CreatePostModule from '@/modules/PostModule/CreatePostModule/CreatePostModule';
+import ProfileNoUser from '@/pages-lib/profile/ProfileNoUser';
 import React from 'react';
 
 export default function AddPostPage() {
+  const { user, loading: userLoading } = useAuth();
+
+  if (userLoading) {
+    return <Spinner />;
+  }
+
+  if (!user) {
+    return (
+      <div className="text-center">
+        <h1>Вы не авторизованы!</h1>
+        <p>Попробуйте перезайти на сайте или перезапустить бота</p>
+      </div>
+    );
+  }
   return <CreatePostModule />;
 }
