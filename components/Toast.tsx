dcd64f034b7502b9e0f328b90886e105730f16ab
Marat Faizerakhmanov <maratismodest@gmail.com>
2024-06-04 20:24:35 +0300

    rid of svg in components

--- components/Toast.tsx
+++ components/Toast.tsx
@@ -1,4 +1,6 @@
 import React, { Dispatch, SetStateAction, useEffect } from 'react';
+import FireSvg from '@/public/svg/fire.svg';
+import CloseSvg from '@/public/svg/close.svg';
 
 const Toast = ({
   toast,
@@ -22,22 +24,8 @@ const Toast = ({
       role="alert"
     >
       <div className="text-blue-500 bg-blue-100 dark:bg-blue-800 dark:text-blue-200 inline-flex h-8 w-8 flex-shrink-0 items-center justify-center rounded-lg">
-        <svg
-          className="h-4 w-4"
-          aria-hidden="true"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 18 20"
-        >
-          <path
-            stroke="currentColor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M15.147 15.085a7.159 7.159 0 0 1-6.189 3.307A6.713 6.713 0 0 1 3.1 15.444c-2.679-4.513.287-8.737.888-9.548A4.373 4.373 0 0 0 5 1.608c1.287.953 6.445 3.218 5.537 10.5 1.5-1.122 2.706-3.01 2.853-6.14 1.433 1.049 3.993 5.395 1.757 9.117Z"
-          />
-        </svg>
-        <span className="sr-only">Fire icon</span>
+        <FireSvg />
+        <span className="sr-only">Иконка огня</span>
       </div>
       <div className="ms-3 text-sm font-normal">Объявление в архиве!</div>
       <button
@@ -47,22 +35,8 @@ const Toast = ({
         aria-label="Close"
         onClick={() => setToast(false)}
       >
+        <CloseSvg />
         <span className="sr-only">Закрыть</span>
-        <svg
-          className="h-3 w-3"
-          aria-hidden="true"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 14 14"
-        >
-          <path
-            stroke="currentColor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
-          />
-        </svg>
       </button>
     </div>
   );
