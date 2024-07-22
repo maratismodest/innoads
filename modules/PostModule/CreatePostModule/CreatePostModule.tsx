6d93e81619c7968090b6d03e096b0fb7b15d2bdd
Marat Faizerakhmanov <maratismodest@gmail.com>
2024-07-22 20:52:43 +0300

    Update CreatePostModule.tsx

--- modules/PostModule/CreatePostModule/CreatePostModule.tsx
+++ modules/PostModule/CreatePostModule/CreatePostModule.tsx
@@ -210,7 +210,7 @@ export function CreatePostModule({ onSubmitOptional = async () => undefined }: P
         </div>
 
         <button
-          className={clsx(buttonStyles({ size: 'medium' }), 'mt-6')}
+          className={clsx(buttonStyles({ size: 'medium' }), 'mx-auto mt-6 w-full md:w-fit')}
           type="submit"
           disabled={loading || categories.length === 0}
           // disabled={true}
