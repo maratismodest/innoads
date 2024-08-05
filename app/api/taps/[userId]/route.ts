f2b875a6e178df9da6ad40591e12ba63980a9851
Marat Faizerakhmanov <marat.faizerakhmanov@ringcentral.com>
2024-08-05 19:47:07 +0300

    Update route.ts

--- app/api/taps/[userId]/route.ts
+++ app/api/taps/[userId]/route.ts
@@ -2,6 +2,7 @@ import { NextResponse } from 'next/server';
 
 import fetchTap from '@/utils/api/prisma/fetchTap';
 
+export const dynamic = 'force-dynamic';
 export async function GET(request: Request, { params }: { params: { userId: string } }) {
   const userId = params.userId;
   const res = await fetchTap(userId ?? '');
